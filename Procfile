c9d3843b50669e7e49f7d3275b3abe9d97756f26
balsubbu2004 <balsubbu2004@gmail.com>
2026-06-23 17:11:23 +0530

    Add production config and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ats_scorer.wsgi --bind 0.0.0.0:$PORT
--- ats_scorer/settings.py
+++ ats_scorer/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tp$bvv0*qf#d@0cc!n9n(fwf!$rzte$er(@%&m)ll$q6g-ywcd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOW_ALL_ORIGINS = True  # fine for development, we'll restrict later
 
